3a8c7ff1dbf3afa2a06c38a661a3c8bc72e41d6f
trainadavide <davide.traina@edu.unifi.it>
2024-05-21 14:48:45 +0200

    first commit

---
+++ PollingAPI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PollingAPI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PollingAPI.settings')
+
+application = get_wsgi_application()
